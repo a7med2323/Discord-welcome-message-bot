2cce3f084fe90606c86f99edb7fb147594c94b89
ahmed <aboa7med23@gmail.com>
2023-08-17 14:37:33 +0200

    edits

--- src/index.js
+++ src/index.js
@@ -20,17 +20,16 @@ client.on('ready',async()=>{
 client.user.setUsername('AGXY');
 GlobalFonts.registerFromPath(path.join(__dirname, '..', 'fonts', 'GONjURING.ttf'), 'conjuring');
 GlobalFonts.registerFromPath(path.join(__dirname, '..', 'fonts', 'Blazed.ttf'), 'blazed');
-console.log(client.guilds.cache.find((guild)=>guild.id === config.serverId))
-guild =  client.guilds.cache.find((guild)=>guild.id === config.serverId);
-//guild.emojis.create({attachment:`${path.join(__dirname,'..','amongus.gif')}`,name:'amongus'}).then(emoji=>console.log('created new emoji')).catch(console.error);
-//console.log(path.join(__dirname, '..', 'fonts', 'GONjURING.ttf'))
+guild =   client.guilds.cache.find((guild)=>guild.id === config.serverId); 
+
+
 });
 
 client.on(Events.GuildMemberAdd, async (member) => {
  
-console.log(member);
 
-  
+
+
   const author =  member.user.username;
   
   const canvas = createCanvas(600,600);
@@ -39,7 +38,7 @@ console.log(member);
   const radius = canvas.height/2;
 
   ctx.save();
-  //ctx.translate(radius,radius);
+  
   ctx.beginPath();
   ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
   ctx.strokeStyle = "yellow";
@@ -49,21 +48,18 @@ console.log(member);
   ctx.drawImage(background,0,0,620,620);
   ctx.restore();
  
-  //const spaceship = await canva.loadImage('./spaceship.png');
+  
   const distance = canvas.width/4;
-  //ctx.drawImage(spaceship, canvas.width/4 , canvas.width/2.15, canvas.width/2 , canvas.height/2);
+  
   ctx.save();
   ctx.beginPath();
   ctx.arc(radius, radius, 80, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.clip(); 
-           //ctx.fillStyle = "red";
-          //ctx.fillRect(0, 0, 150, 100);
-              //const background = await canva.loadImage('./wallpaper.png');
+           
  const useravatar =  member.displayAvatarURL({ extension: 'png' });
  const avatarload = await loadImage(useravatar);
- //const avatarload = await canva.loadImage('./wallpaper.png');
-             //const backtopng = await background.encode('png');
+
  ctx.drawImage(avatarload, radius-100, radius-100, 200, 200);
  ctx.restore();
   ctx.font = "28px blazed";
@@ -73,29 +69,8 @@ console.log(member);
   ctx.fillStyle = grd;
   ctx.textAlign = "center";
   ctx.fillText(` ${author}`, canvas.width/2 , 450);
-  /*
-  ctx.fillStyle = "#D5A446";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = "90px Comic Sans MS";
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-  ctx.fillText(`welcome ${author}`, canvas.width/2, canvas.height/2);
-   //ctx.rect(100, 500, 500, 120);
-   ctx.save();
-   ctx.beginPath();
-   ctx.arc(109, 109, 100, 0, 2 * Math.PI);
-   ctx.stroke();
-   ctx.clip(); 
-//ctx.fillStyle = "red";
-//ctx.fillRect(0, 0, 150, 100);
-  //const background = await canva.loadImage('./wallpaper.png');
-  const background = await message.author.displayAvatarURL({ extension: 'png' });
-  const backgroundload = await canva.loadImage(background);
-    //const backtopng = await background.encode('png');
-  ctx.drawImage(backgroundload, 9, 9, 200, 200);
-  ctx.restore();*/
-  //canvas.toBuffer()
   
+
   const attachment =  new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
   const channel = await client.channels.cache.get(config.welcomechannel);
   const iconurl = await guild.iconURL();
@@ -107,7 +82,7 @@ console.log(member);
 		{ name: '✰.｡.✵°✵welcome', value: member.user.username },
 	)
   .setImage('attachment://profile-image.png')
-	.setFooter({ text: '▬▬▬▬▬▬▬▬\n@2023 AGXY STORE', iconURL: iconurl });
+	.setFooter({ text: `▬▬▬▬▬▬▬▬\n@2023 ${guild}`, iconURL: iconurl });
 
 
 
